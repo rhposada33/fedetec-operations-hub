5864eb49ddc41673437b6590d89a12377fbce8e8
ViraLata <rafaelposada@gmail.com>
2026-05-29 13:58:18 -0300

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 export default defineConfig({
   vite: {
     server: {
-      allowedHosts: [".ngrok-free.app"],
+      allowedHosts: [".ngrok-free.app", "rafa-pc.tail7ed83d.ts.net"],
       proxy: {
         "/api": {
           target: "http://localhost:8000",
